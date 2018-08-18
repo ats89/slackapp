fdec2792d686896d0972f01b2d69bf1177a20b9b
Anthony Sin <anthony.t.sin@gmail.com>
2018-08-18 15:13:03 -0400

    added git-help slash command

--- app/routes.js
+++ app/routes.js
@@ -44,9 +44,6 @@ module.exports = (app) => {
   // Slash command '/select-student' 
   app.post('/select-student', (req, res) => {
     const input = req.body.text;
-
-    console.log(input);
-
     const response_url = req.body.response_url;   
 
     res.json({
@@ -59,5 +56,49 @@ module.exports = (app) => {
     }).catch((err) => {
       console.log('getUsersList() error: ', err);
     });
+  });
+
+  // Slash command '/git-help'
+  app.post('/git-help', (req, res) => {
+    const input = req.body.text;
+    const response_url = req.body.response_url;
+    const attachments = [
+      {
+        "fallback": "git help 1",
+        "color": "#de3C4b",
+        "text": "```git status # view state of repo\ngit add <some_file> # stage a file\ngit add . / git add -A # add all changed files to staging area\ngit commit -m “...”```",
+        "mrkdwn_in": ["text"]
+      },
+      {
+        "fallback": "git help 2",
+        "color": "#390099",
+        "text": "```git branch # list branches, show current branch (*)\ngit checkout master # switch to master\ngit checkout <branch_name> # switch to branch_name\ngit checkout -b <new_branch> # create and switch to new_branch```",
+        "mrkdwn_in": ["text"]
+      },
+      {
+        "fallback": "git help 3",
+        "color": "#65Def1",
+        "text": "```git push origin <my-branch> # push branch to remote repo\ngit pull origin <my-branch> # pull latest changes from branch (might be necessary if other people worked on branch)\ngit pull origin master # pull latest changes from master\n# then, -> fix conflicts -> push branch -> pull request -> review -> merge on GitHub (since we set master to be protected)\ngit branch -d <my-branch> # delete branch after it is merged; -D to force the delete```",
+        "mrkdwn_in": ["text"]
+      },
+      {
+        "fallback": "git help 4",
+        "color": "#c4f7a1",
+        "text": "```git branch -r # see list of remote branches (other people’s branches that have been pushed)\ngit fetch -p # keep local repo in sync with deleted remote branches (branches deleted on Github won’t show when running git branch -r)\ngit checkout <remote-branch> # make remote branch a local branch to work on (work on someone else’s branch)```",
+        "mrkdwn_in": ["text"]
+      },
+    ];
+
+    if (input === 'channel') {
+      res.json({
+        "response_type": "in_channel",
+        "attachments": attachments, 
+      });
+    } else {
+      res.json({
+        "response_type": "ephermeral", 
+        "attachments": attachments, 
+      });
+    }
   });
 }
