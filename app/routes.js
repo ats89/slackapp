1da1ee6830be98656499a0610cf40f6f0c86105f
Anthony Sin <anthony.t.sin@gmail.com>
2018-09-16 19:41:07 -0400

    reformat 3

--- app/routes.js
+++ app/routes.js
@@ -66,23 +66,29 @@ module.exports = (app) => {
       {
         "fallback": "git help 1",
         "color": "#de3C4b",
-        "text": "```git status # view state of repo\ngit add <some_file> # stage a file\ngit add . / git add -A # add all changed files to staging area\ngit commit -m “...”\ngit stash # saves local commits and reverts working directory\ngit stash apply # restores saved commits and applies them to current branch```",
+        "text": "```git status # view state of repo\ngit add <some_file> # stage a file\ngit add . / git add -A # add all changed files to staging area\ngit commit -m “...”```",
         "mrkdwn_in": ["text"]
       },
       {
         "fallback": "git help 2",
+        "color": "#ffa500",
+        "text": "```git stash # saves local commits and reverts working directory\ngit stash apply # restores saved commits and applies them to current branch```",
+        "mrkdwn_in": ["text"]
+      },
+      {
+        "fallback": "git help 3",
         "color": "#390099",
         "text": "```git branch # list branches, show current branch (*)\ngit checkout master # switch to master\ngit checkout <branch_name> # switch to branch_name\ngit checkout -b <new_branch> # create and switch to new_branch```",
         "mrkdwn_in": ["text"]
       },
       {
-        "fallback": "git help 3",
+        "fallback": "git help 4",
         "color": "#65Def1",
         "text": "```git push origin <my-branch> # push branch to remote repo\ngit pull origin <my-branch> # pull latest changes from branch (might be necessary if other people worked on branch)\ngit pull origin master # pull latest changes from master\n# then, -> fix conflicts -> push branch -> pull request -> review -> merge on GitHub (since we set master to be protected)\ngit branch -d <my-branch> # delete branch after it is merged; -D to force the delete```",
         "mrkdwn_in": ["text"]
       },
       {
-        "fallback": "git help 4",
+        "fallback": "git help 5",
         "color": "#c4f7a1",
         "text": "```git branch -r # see list of remote branches (other people’s branches that have been pushed)\ngit fetch -p # keep local repo in sync with deleted remote branches (branches deleted on Github won’t show when running git branch -r)\ngit checkout <remote-branch> # make remote branch a local branch to work on (work on someone else’s branch)```",
         "mrkdwn_in": ["text"]
